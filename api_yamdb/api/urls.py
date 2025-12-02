b63e6e69e13070a26ea8f7876ffb406dc965ada7
vodoleikin <vodoleikin@yandex.ru>
2025-12-02 11:13:54 +0300

    02.12.2025. 11.10. v2. 74 tests passed, 6 tests failed. Corrects some files.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,19 +1,45 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from ..reviews import views
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
 
-router = DefaultRouter()
+from .views import (APIGetToken, APISignup, CategoryViewSet, CommentViewSet,
+                    GenreViewSet, ReviewViewSet, TitleViewSet, UsersViewSet)
+
+app_name = 'api'
+
+router = SimpleRouter()
 router.register(
     r'titles/(?P<title_id>\d+)/reviews',
-    views.ReviewViewSet,
-    basename='review'
+    ReviewViewSet,
+    basename='reviews'
 )
 router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    views.CommentViewSet,
-    basename='comment'
+    CommentViewSet,
+    basename='comments'
+)
+router.register(
+    'users',
+    UsersViewSet,
+    basename='users'
+)
+router.register(
+    'categories',
+    CategoryViewSet,
+    basename='сategories'
+)
+router.register(
+    'titles',
+    TitleViewSet,
+    basename='titles'
+)
+router.register(
+    'genres',
+    GenreViewSet,
+    basename='genres'
 )
 
 urlpatterns = [
+    path('v1/auth/token/', APIGetToken.as_view(), name='get_token'),
     path('v1/', include(router.urls)),
+    path('v1/auth/signup/', APISignup.as_view(), name='signup'),
 ]
