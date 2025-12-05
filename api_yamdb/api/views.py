aec3ccd7a9071bdcd57101f28efdbfaf991417d1
Nikita <nikkit1012@gmail.com>
2025-12-05 13:34:57 +0500

    veiw fix

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -70,20 +70,26 @@ class APIGetToken(APIView):
     def post(self, request):
         serializer = GetTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        data = serializer.validated_data
+
+        username = serializer.validated_data.get('username')
+        confirmation_code = serializer.validated_data.get('confirmation_code')
+
         try:
-            user = User.objects.get(username=data['username'])
+            user = User.objects.get(username=username)
         except User.DoesNotExist:
             return Response(
-                {'username': 'Пользователь не найден!'},
-                status=status.HTTP_404_NOT_FOUND)
-        if data.get('confirmation_code') == user.confirmation_code:
-            token = RefreshToken.for_user(user).access_token
-            return Response({'token': str(token)},
-                            status=status.HTTP_201_CREATED)
+                {"username": "Пользователь не найден"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        if default_token_generator.check_token(user, confirmation_code):
+            token = str(RefreshToken.for_user(user).access_token)
+            return Response({"token": token}, status=status.HTTP_200_OK)
+
         return Response(
-            {'confirmation_code': 'Неверный код подтверждения!'},
-            status=status.HTTP_400_BAD_REQUEST)
+            {"confirmation_code": "Неверный код подтверждения!"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 
 class APISignup(APIView):
@@ -103,40 +109,34 @@ class APISignup(APIView):
 
     def post(self, request):
         serializer = SignUpSerializer(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors,
-                            status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
 
         email = serializer.validated_data['email']
         username = serializer.validated_data['username']
 
-        try:
-            user = User.objects.get(email=email, username=username)
-            user.confirmation_code = self.generate_confirmation_code(user)
-            user.save()
-        except User.DoesNotExist:
-            user = User.objects.create_user(
-                username=username,
-                email=email,
-                password=None
-            )
-            user.confirmation_code = self.generate_confirmation_code(user)
-            user.save()
+        user, created = User.objects.get_or_create(
+            username=username,
+            email=email
+        )
+
+        confirmation_code = self.generate_confirmation_code(user)
 
         email_body = (
             f'Доброе время суток, {user.username}.\n'
-            f'Код подтверждения для доступа к API: {user.confirmation_code}'
+            f'Код подтверждения для доступа к API: {confirmation_code}'
         )
-        data = {
+
+        self.send_email({
             'email_body': email_body,
             'to_email': user.email,
             'email_subject': 'Код подтверждения для доступа к API!'
-        }
-        self.send_email(data)
+        })
 
         return Response(
-            {'email': user.email, 'username': user.username},
+            {
+                "email": user.email,
+                "username": user.username
+            },
             status=status.HTTP_200_OK
         )
 
@@ -197,61 +197,28 @@ class TitleViewSet(ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAdminModeratorAuthorPermission,)
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
+
+    def get_review(self):
+        return get_object_or_404(Review, id=self.kwargs.get('review_id'))
 
     def get_queryset(self):
-        review = get_object_or_404(
-            Review,
-            id=self.kwargs.get('review_id'))
-        return review.comments.all()
+        return self.get_review().comments.all()
 
     def perform_create(self, serializer):
-        review = get_object_or_404(
-            Review,
-            id=self.kwargs.get('review_id'))
-        serializer.save(author=self.request.user, review=review)
-
-    def update(self, request, *args, **kwargs):
-        return Response(
-            {"detail": "Метод PUT не поддерживается для комментариев."},
-            status=status.HTTP_405_METHOD_NOT_ALLOWED
-        )
-
-    def partial_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.save(author=self.request.user, review=self.get_review())
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = (IsAdminModeratorAuthorPermission,)
+    http_method_names = ['get', 'post', 'patch', 'delete', 'head', 'options']
+
+    def get_title(self):
+        return get_object_or_404(Title, id=self.kwargs.get('title_id'))
 
     def get_queryset(self):
-        title = get_object_or_404(
-            Title,
-            id=self.kwargs.get('title_id'))
-        return title.reviews.all()
+        return self.get_title().reviews.all()
 
     def perform_create(self, serializer):
-        title = get_object_or_404(
-            Title,
-            id=self.kwargs.get('title_id'))
-        serializer.save(author=self.request.user, title=title)
-
-    def update(self, request, *args, **kwargs):
-        return Response(
-            {"detail": "Метод PUT не поддерживается для отзывов. "},
-            status=status.HTTP_405_METHOD_NOT_ALLOWED
-        )
-
-    def partial_update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer.save(author=self.request.user, title=self.get_title())
